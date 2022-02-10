071cfce28f354f0fdf0a1101cae8737ba72ef364
nhinguyen26 <nhinguyen1326@yahoo.com>
2022-02-09 16:49:33 -0800

    counterapp scratch

---
+++ CounterApp/CounterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CounterApp
+//
+//  Created by Nhi Nguyen on 2/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Vstack(alignment: .leading) {
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ CounterApp/CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Nhi Nguyen on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
